9ad3fff6ef2e97981047020b95318b4bc5396426
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-06-19 22:12:41 +0100

    Import font and draw paddles/ball

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ WINDOW_WIDTH, WINDOW_HEIGHT = love.graphics.getDimensions()
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    retroFont = love.graphics.newFont('src/assets/fonts/font.ttf', 8)
+    love.graphics.setFont(retroFont)
     Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -15,7 +17,11 @@ end
 
 function love.draw()
     Push:apply('start')
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 1)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
     Push:apply('end')
 end
 
